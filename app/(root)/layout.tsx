e7d44114496afa1dbcda09fefd8a4c200d5de979
khaildalhadil <khaild.alhadi2020@gamil.com>
2026-03-23 19:35:25 +0400

    feat: initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,23 @@
+import Footer from "@/components/footer";
+import Header from "../../components/shared/header";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <div className="flex h-screen flex-col">
+
+      <Header />
+
+      <main className="flex-1 wrapper">
+        second lqyout
+        {children}
+      </main>
+      
+      <Footer />
+    </div>
+  );
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+// export const metadata = {
+//   title: 'Home'
+// }
+
+export default function Page() {
+  return (
+    <div>page Hello;</div>
+  )
+}
